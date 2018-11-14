4d1632e2a735a774a19bdbe10897dc9472fb2f81
john Doe <jeffreyalmonte@Pursuits-MacBook-Pro-106.local>
2018-11-14 14:28:54 -0500

    auto layout lab

---
+++ AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Jeffrey Almonte on 11/14/18.
+//  Copyright © 2018 Jeffrey Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
